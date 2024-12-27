6e8013e08754069290f0ee203a7c0556d9e3ca32
lucas-bernardino <lucasgber@gmail.com>
2024-12-27 18:20:40 -0300

    Initial structure of home

---
+++ frontend/src/app/(pages)/layout.tsx
@@ -0,0 +1,10 @@
+export default function PageLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative flex justify-center item-start w-[1200px] m-auto">
+      <div className="sticky top-0 flex mx-4 max-w-[200px]">MENU
+      </div>
+      {children}
+      <div className="sticky top-0 flex w-full max-w-[320px]">Busca</div>
+    </div>
+  )
+}
